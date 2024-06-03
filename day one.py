d30a48398a20e017de2acc3b1729e33214ba6266
Prasannnnn <prasannamb4362@gmail.com>
2024-06-03 13:16:13 +0530

    day one codes

---
+++ day one.py
@@ -0,0 +1,70 @@
+#python is static or dynamic
+
+# # C Language
+# int a = 10
+# int b = 20
+# int c =
+#python language
+# a = 10
+# b = 20
+# c = a+b
+# print(c)
+
+'''
+int , float, complex ---> Integer type
+str                  ---> text type
+list, tuple, range   ---> Sequence type
+dict                 ---> Mapping
+set, frozenset       ---> set types
+bool                 ---> Boolean types
+Binary types         ---> bytes, bytearray, memoryview
+None type            ---> None
+'''
+
+A = memoryview(bytes(5)) 
+print(A)
+
+# b = b"Hello"
+# print(b)
+# print(type(b))
+'''
+Arithmetic Operator:
+    + , - ,*, /,//,%,**
+Assignment Operator:
+    a += 5 ---> a = a+5
+Comparison Operator:
+    == equal to
+    != Not equal to
+    < less
+    > greater
+    <= lesser than equal to
+    >= greater than equal to
+Logical Operator:
+    AND 
+    OR
+    NOT
+Bitwise Operator:
+    &&
+    |
+    `
+Identity Operator:
+    is 
+    is not
+Membership Operator:
+    in 
+    not in 
+'''
+#Arithmetic Operators
+a = int (input("Enter the value"))
+b = int (input("Enter the b value"))
+# print(a+b)#addition
+# print(a-b)#subtraction
+# print(a*b)#multip
+# print(a/b)
+# print(a//b)
+# print(a%b)
+# print(a**b)
+
+#Assignment operator
+a //=b
+print(a)
