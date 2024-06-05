4c0efa59ba359d5b1756a05576ddadef8c3dd9b1
Prasannnnn <prasannamb4362@gmail.com>
2024-06-05 12:56:22 +0530

    added day 3

---
+++ day three.py
@@ -0,0 +1,95 @@
+# a = {
+#     "name":"hema",
+#     "age":24,
+#     "Role":'studying',
+#     "College":'CMC College'
+#     }
+# # print(a)---> Dictionary callable
+# #access of dictionaries
+# # b = a["name"]
+# # c = a["age"]
+# #using get
+# b=a.get('College')
+# print(b)
+
+# #keys return a list of all the keys in a dictionary
+
+# c = a.keys()
+# print(c)
+
+# a["area"]="Arani"
+# print(a)
+
+# '''
+# Get values
+
+# ---> c = a.values()
+# generally print the values of the dictionary
+# '''
+# d=a.values()
+# print(d)
+
+# '''
+# Get items
+# return each item in dictionary
+# '''
+
+# # e = a.items()
+# # print("items",e)
+
+# # a["area"]="vellore"
+
+# # print(a)
+
+# '''
+# update dictionary
+# '''
+# a.update({'area':'Vadalur'})
+
+# print(a)
+
+
+# '''
+# Loop dictionaries
+# '''
+# for x in a:
+#     print(x)
+
+# fri = {
+#     "a" : {
+#         "name":"Pradeep",
+#         "age":22
+#     },
+#     "b" :{
+#         "name":"Pranash",
+#         "age":23
+#     }
+# }
+# print(fri["a"]["age"])
+
+
+
+'''
+odd ---> weird
+even range (6,20) ---> not weird
+even greater (20)---> not weird
+'''
+
+def add(a,b):
+    return a+b
+print(add(10,5))
+
+'''
+user two inputs a and b
+Arithmetic Operations +,-,*,/,//,%,**
+a = 10
+b = 5
+operators = add
+15
+Use Functions
+'''
+
+'''
+Functions
+'''
+
