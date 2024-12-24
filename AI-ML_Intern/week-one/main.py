84a9e69c043657f2f1cee731f931bd4507fd082d
prasannnnn <prasannamb4362@gmial.com>
2024-12-24 10:46:07 +0530

    work

---
+++ AI-ML_Intern/week-one/main.py
@@ -0,0 +1,6 @@
+a = int(input("Enter the a Value"))
+b = int(input("Enter the b value"))
+c = a+b
+print("the addition of ", a ,"and",b,"is",c)
+print(c)
+print(f"the addition of {a} and {b} is {c}")
